13cc1979a60c8945f85544d1daa7425f10b5ee99
ArturHatoka <hameleon2011007@mail.ru>
2019-09-09 12:46:10 +0300

    mailed

--- src/mail/send-mail.php
+++ src/mail/send-mail.php
@@ -1,19 +1,23 @@
 <?php
 $name = $_POST['name'];
 $tel = $_POST['tel'];
+$text = $_POST['text'];
 
 $name = htmlspecialchars($name);
 $tel = htmlspecialchars($tel);
+$text = htmlspecialchars($text);
 
 $name = urldecode($name);
 $tel = urldecode($tel);
+$text = urldecode($text);
 
 $name = trim($name);
 $tel = trim($tel);
+$text = trim($text);
 
 if (mail("hameleon2011007@mail.ru",
     "Заявка с сайта",
-    "Имя: ".$name. "\nТелефон: ".$tel ))
+    "Имя: ".$name. "\nТелефон: ".$tel. "\nСообщение: \n".$text ))
 {
     header("Location: /#endthx");
 } else {
